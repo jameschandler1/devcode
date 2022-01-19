82211eef9413d760bef75e1ebaca992d0c3d05a8
James Chandler <stillsound@protonmail.com>
2022-01-18 18:11:23 -0800

    add export to store.js

--- client/src/store.js
+++ client/src/store.js
@@ -7,4 +7,8 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(
+    rootReducer, 
+    initialState, composeWithDevTools(applyMiddleware(...middleware)));
+
+export default store
