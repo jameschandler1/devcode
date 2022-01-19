bef70bf99861456bef9ef565d68e31fdf8be39d8
James Chandler <stillsound@protonmail.com>
2022-01-18 22:34:34 -0800

    add redirect once successful login attempt is made

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,10 +2,10 @@ import React, { Fragment, useState } from "react";
 import { connect } from "react-redux";
 import { setAlert } from "../../actions/alert";
 import { register } from "../../actions/auth";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -30,6 +30,10 @@ const Register = ({ setAlert, register }) => {
       })
     }
   };
+  
+    if (isAuthenticated) {
+        return <Redirect to="/dashboard" />;
+    }
 
   return (
     <Fragment>
@@ -97,6 +101,11 @@ const Register = ({ setAlert, register }) => {
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
   register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
 };
 
-export default connect(null, { register, setAlert })(Register);
+const mapStateToProps = (state) => ({
+    isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(null, { register, setAlert, mapStateToProps })(Register);
