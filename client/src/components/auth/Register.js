43d53ebffc2690e9ff5ed6fd0a48e30909b78857
James Chandler <stillsound@protonmail.com>
2022-01-18 20:04:32 -0800

    create reducers for auth for removing and adding token based on auth status

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,11 @@
 import React, { Fragment, useState } from "react";
-
 import { connect } from "react-redux";
 import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -23,7 +23,11 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert("Passwords do not match!", "danger");
     } else {
-      console.log("success");
+      register({
+        name,
+        email,
+        password,
+      })
     }
   };
 
@@ -42,7 +46,7 @@ const Register = ({ setAlert }) => {
             name="name"
             value={name}
             onChange={(e) => onChange(e)}
-            required
+            // required
           />
         </div>
         <div className="form-group">
@@ -65,7 +69,7 @@ const Register = ({ setAlert }) => {
             type="password"
             placeholder="Password"
             name="password"
-            minLength="6"
+            // minLength="6"
             value={password}
             onChange={(e) => onChange(e)}
           />
@@ -76,7 +80,7 @@ const Register = ({ setAlert }) => {
             type="password"
             placeholder="Confirm Password"
             name="password2"
-            minLength="6"
+            // minLength="6"
             value={password2}
             onChange={(e) => onChange(e)}
           />
@@ -92,6 +96,7 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { register, setAlert })(Register);
