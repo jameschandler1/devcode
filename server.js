09ee0604e14b16f76f7a09d29778d7b3b8a0261c
James Chandler <stillsound@protonmail.com>
2022-01-21 11:10:56 -0800

    settings for production

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const connectdb = require('./config/db');
-
+const express = require("express");
+const connectdb = require("./config/db");
+const path = require("path");
 const app = express();
 
 //connect database
@@ -9,18 +9,22 @@ connectdb();
 // middleware
 app.use(express.json({ extended: false }));
 
-
-app.get('/', (req, res) => res.send(`API is running`));
-
 //routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+//serve static files for production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`);
-})
+  console.log(`Server running at port ${PORT}`);
+});
