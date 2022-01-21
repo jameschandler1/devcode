0a98f1876967035735e1b57a573cd82658360452
James Chandler <stillsound@protonmail.com>
2022-01-21 11:24:34 -0800

    specify settings in package.json

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { getPost } from '../../actions/post';
 import Moment from 'react-moment';
 import { deleteComment } from '../../actions/post';
 
